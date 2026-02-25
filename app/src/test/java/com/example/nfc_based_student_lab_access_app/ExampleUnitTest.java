12ad3bb3db4301f7d1c87acffb2130c8de061198
Zineb Bamouh <zineb.bamouh@gmail.com>
2026-02-24 22:25:01 -0500

    Initial Android project

---
+++ app/src/test/java/com/example/nfc_based_student_lab_access_app/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.example.nfc_based_student_lab_access_app;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
